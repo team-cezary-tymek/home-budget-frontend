4123c32d68d09c0f0a8d2e2b38673d2329f51e31
jagtym <jagtymxd@gmail.com>
2023-01-24 22:34:33 +0100

    Add sidenav and basic layout

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -12,12 +12,15 @@ import { MatButtonModule } from '@angular/material/button';
 import { MatDialogModule } from '@angular/material/dialog';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input';
+import { MatSidenavModule } from '@angular/material/sidenav';
 import { FormsModule } from '@angular/forms';
 import { ReactiveFormsModule } from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
 import { DashboardComponent } from './dashboard/dashboard.component';
 import { RouterModule } from '@angular/router';
-
+import { MatListModule } from '@angular/material/list';
+import {MatIcon, MatIconModule} from '@angular/material/icon';
+import { MatToolbarModule } from '@angular/material/toolbar';
 
 @NgModule({
     declarations: [
@@ -30,11 +33,15 @@ import { RouterModule } from '@angular/router';
         BrowserModule,
         BrowserAnimationsModule,
         MatSlideToggleModule,
+        MatToolbarModule,
         MatTableModule,
+        MatIconModule,
         MatSortModule,
         MatButtonModule,
         MatDialogModule,
         MatFormFieldModule,
+        MatSidenavModule,
+        MatListModule,
         MatInputModule,
         FormsModule,
         ReactiveFormsModule,
