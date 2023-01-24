92602b0b8ef2b558716bb771e468c747714acc1b
jagtym <jagtymxd@gmail.com>
2023-01-24 22:45:56 +0100

    Add sidenav toggle button function

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,9 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
     title = 'home-budget';
+    showSidenav: boolean = true;
+
+    onToggle(): void {
+        this.showSidenav = !this.showSidenav;
+    }
 }
