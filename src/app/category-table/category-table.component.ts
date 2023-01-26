566afab23d7e070ea4f585f5922fc3a07d2bd86d
jagtym <jagtymxd@gmail.com>
2023-01-26 22:27:40 +0100

    Fix errors after merge

--- src/app/category-table/category-table.component.ts
+++ src/app/category-table/category-table.component.ts
@@ -41,6 +41,8 @@ export class CategoryTableComponent {
             id: category.id,
             name: category.name,
             description: category.description,
+            created_at: category.created_at,
+            updated_at: category.updated_at,
         };
     }
 
