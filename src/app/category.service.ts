bb4852e667ccc54974ae578e20914dd34b1cc4e2
CzarsonZiomal <szwedekc@gmail.com>
2023-01-26 14:31:53 +0100

    main

--- src/app/category.service.ts
+++ src/app/category.service.ts
@@ -1,9 +1,21 @@
 import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { Category } from './category';
 
 @Injectable({
   providedIn: 'root'
 })
 export class CategoryService {
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
+
+    private expensesUrl = 'http://127.0.0.1:3000/categories'
+
+    getCategories(): Observable<Category[]> {
+        this.http.get<any>(this.expensesUrl).subscribe(data => {
+            console.log(data);
+        })
+        return this.http.get<any>(this.expensesUrl)
+    }
 }
